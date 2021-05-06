bbb24d7619c33126dbf231498fbade5e2cc4ddb6
Saharsh-R <48884865+Saharsh-R@users.noreply.github.com>
2021-05-06 21:35:19 +0530

    ip

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,73 @@
 'use strict';
 
+const fetch = require("node-fetch");
+
+
+require('dotenv').config()
+let mongoose = require("mongoose");
+mongoose.pluralize(null);
+mongoose.connect(process.env.DB, { useNewUrlParser: true, useUnifiedTopology: true ,useFindAndModify: false});
+// create schema
+// Database = mongoose.model('ipadd', )
+
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-      
+      console.log(req.connection.remoteAddress)
+      let {stock, like} = req.query
+      if (Array.isArray(stock)){
+
+        fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock[0]}/quote`)
+          .then(res => res.json())
+          .then(data => {
+            if (typeof data == 'string'){
+              return res.send('First stock is not valid')
+            } else {
+
+              fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock[1]}/quote`)
+                .then(res => res.json())
+                .then(data2 => {
+                  if (typeof data2 == 'string'){
+                    return res.send('Second stock is not valid')
+                  } else {
+
+                    return res.send({stockData: [
+                      {
+                        stock: data.symbol,
+                        price: data.latestPrice,
+                        rel_likes: 0
+                      },
+                      {
+                        stock: data2.symbol,
+                        price: data2.latestPrice,
+                        rel_likes: 0
+                      }
+                    ]})
+                  }
+                })
+                .catch(e => console.error(e))
+            }
+          })
+          .catch(e => console.error(e))
+        
+      } else {
+
+        fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stock}/quote`)
+          .then(res => res.json())
+          .then(data => {
+            if (typeof data == 'string'){
+              return res.send('Please enter a valid stock')
+            } else {
+              return res.send({stockData: {
+                stock: data.symbol,
+                price: data.latestPrice,
+                likes: 0
+              }})
+            }
+          })
+          .catch(e => console.error(e))
+      }
     });
     
 };
